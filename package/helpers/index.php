970eab83350c5ff8666993a7871e69d5c35013da
João Pedro Garcia <jotapegue09@gmail.com>
2024-07-18 16:13:02 -0300

    copy helpers

---
+++ package/helpers/index.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Jotapegue\Phpxform\helpers;
+
+use Exception;
+use InvalidArgumentException;
+
+// if (!function_exists('slugify')) {
+//     function slugify($string) : string {
+//         return preg_replace('/\W+/', '_', trim(strtolower($string)));
+//     }
+// }
+
+if (!function_exists('dd')) {
+    function dd() : never {
+        var_dump(...func_get_args());
+        die();
+    }
+}
+
+if(!function_exists('app_base'))
+{
+    function app_base(?string $target = null)
+    {
+        $pathToTarget = implode(DIRECTORY_SEPARATOR, [getcwd(), $target]);
+
+        if (!file_exists($pathToTarget) && !is_dir($pathToTarget)) {
+            throw new InvalidArgumentException('Path or file does not exist');
+        }
+
+        return is_null($target)
+            ? getcwd()
+            : $pathToTarget;
+
+    }   
+}
+
+
+
